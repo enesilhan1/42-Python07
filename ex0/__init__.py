3823dcfac26d7709cf4190b3ec53ab5329ea2c9a
enesilhan1 <enesilhan924@gmail.com>
2026-08-28 22:32:58 +0300

    build: __init__.py directory has been created and completed

---
+++ ex0/__init__.py
@@ -0,0 +1,11 @@
+from .factory import CreatureFactory
+from .factory import FlameFactory
+from .factory import AquaFactory
+from .creature import Creature
+
+__all__ = [
+    "FlameFactory",
+    "AquaFactory",
+    "CreatureFactory",
+    "Creature"
+]
