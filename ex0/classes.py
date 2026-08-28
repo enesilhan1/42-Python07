58147b0e66061df82568b7b8e1926f0344be851c
enesilhan1 <enesilhan924@gmail.com>
2026-08-28 14:34:17 +0300

    add: Pyrodon class has been created

--- ex0/classes.py
+++ ex0/classes.py
@@ -22,3 +22,12 @@ class Flameling(Creature):
     def attack(self) -> str:
         return (f"{self.name} uses Ember!")
 
+
+class Pyrodon(Creature):
+    def __init__(self) -> None:
+        super().__init__("Pyrodon","Fire/Flying")
+
+    def attack(self) -> str:
+        return (f"{self.name} uses Flamethrower!")
+
+
