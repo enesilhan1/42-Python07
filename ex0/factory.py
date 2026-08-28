933d810ab8fabdadda49c39ef6dc61221a2cd9a7
enesilhan1 <enesilhan924@gmail.com>
2026-08-28 22:25:48 +0300

    add: the AquaFactory class has been created

--- ex0/factory.py
+++ ex0/factory.py
@@ -19,3 +19,11 @@ class FlameFactory(CreatureFactory):
 
     def create_evolved(self) -> creature.Creature:
         return creature.Pyrodon()
+
+
+class AquaFactory(CreatureFactory):
+    def create_base(self) -> creature.Creature:
+        return creature.Aquabub()
+
+    def create_evolved(self) -> creature.Creature:
+        return creature.Torragon()
