5cc46483d3488bd544a612a1695d8a1f45f78760
enesilhan1 <enesilhan924@gmail.com>
2026-08-28 14:41:57 +0300

    add: Torragon class created

--- ex0/classes.py
+++ ex0/classes.py
@@ -35,5 +35,14 @@ class Aquabub(Creature):
     def __init__(self) -> None:
         super().__init__("Aquabub", "Water")
 
-    def attack(self):
+    def attack(self) -> str:
         return (f"{self.name} uses Water Gun!")
+
+
+class Torragon(Creature):
+    def __init__(self):
+        super().__init__("Torragon", "Water")
+
+
+    def attack(self) -> str:
+        return (f"{self.name} uses HYdro Pump!")
