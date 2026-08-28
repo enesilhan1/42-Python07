8d12c7eb39f41cf7748be9f6c82ab7259b41beb7
enesilhan1 <enesilhan924@gmail.com>
2026-08-28 22:55:04 +0300

    fix: in the __init__.py import creature line has been deleted bc it was useless

--- ex0/__init__.py
+++ ex0/__init__.py
@@ -1,11 +1,9 @@
 from .factory import CreatureFactory
 from .factory import FlameFactory
 from .factory import AquaFactory
-from .creature import Creature
 
 __all__ = [
     "FlameFactory",
     "AquaFactory",
     "CreatureFactory",
-    "Creature"
 ]
