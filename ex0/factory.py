eece6f22337911e23b22ceba491390a96a16529d
enesilhan1 <enesilhan924@gmail.com>
2026-08-28 22:23:59 +0300

    add: FlameFactory class has been created and added

--- ex0/factory.py
+++ ex0/factory.py
@@ -5,9 +5,17 @@ from . import creature
 class CreatureFactory(abc.ABC):
 
     @abc.abstractmethod
-    def create_base(self) -> creature.Creatures:
+    def create_base(self) -> creature.Creature:
         pass
 
     @abc.abstractmethod
-    def create_evolved(self) -> creature.Creatures:
+    def create_evolved(self) -> creature.Creature:
         pass
+
+
+class FlameFactory(CreatureFactory):
+    def create_base(self) -> creature.Creature:
+        return creature.Flameling()
+
+    def create_evolved(self) -> creature.Creature:
+        return creature.Pyrodon()
