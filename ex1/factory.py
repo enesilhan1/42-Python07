d785a8bd093803500517077e41af249b2d4245de
enesilhan1 <enesilhan924@gmail.com>
2026-09-04 16:39:37 +0300

    build: HealingCretureFActory has been created and completed in the factory file

---
+++ ex1/factory.py
@@ -0,0 +1,15 @@
+from ex0 import CreatureFactory
+from ex0.creature import Creature
+from ex1.creatures import Sproutling
+from ex1.creatures import Bloomelle
+from ex1.creatures import Morphagon
+from ex1.creatures import Shiftling
+
+
+class HealingCreatureFactory(CreatureFactory):
+    def create_base(self) -> Creature:
+        return Sproutling()
+
+    def create_evolved(self) -> Creature:
+        return Bloomelle()
+
