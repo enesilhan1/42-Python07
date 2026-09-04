bd0b30856ab0400b13bdc76bffd7324b6cbc58e6
enesilhan1 <enesilhan924@gmail.com>
2026-09-04 16:41:01 +0300

    add: TransformCreatureFactory has been created and completed

--- ex1/factory.py
+++ ex1/factory.py
@@ -13,3 +13,9 @@ class HealingCreatureFactory(CreatureFactory):
     def create_evolved(self) -> Creature:
         return Bloomelle()
 
+class TransformCreatureFactory(CreatureFactory):
+    def create_base(self) -> Creature:
+        return Shiftling()
+
+    def create_evolved(self) -> Creature:
+        return Morphagon()
