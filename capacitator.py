f70b192e90efa3d286ca227e5c35c1119d525d72
enesilhan1 <enesilhan924@gmail.com>
2026-09-04 17:34:45 +0300

    capatitator fie has been created but its not done yet

--- capacitator.py
+++ capacitator.py
@@ -1,1 +1,27 @@
+from ex1 import creatures
+from ex1 import capacitor
+from ex1 import HealingCreatureFactory
+from ex1 import TransformCreatureFactory
 
+
+def healing(fac: HealingCreatureFactory) -> None:
+    base = fac.create_base()
+    print("Testing Creature with healing capability")
+    print(" base:")
+
+    print(base.describe())
+    print(base.attack())
+    print(base.heal())
+
+
+
+
+def main() -> None:
+    heal = HealingCreatureFactory()
+    trans = TransformCreatureFactory()
+
+    healing(heal)
+
+
+if __name__ == "__main__":
+    main()
