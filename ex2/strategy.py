d941719ad0fa5c3103bc2e1007edfd0462a6e6ba
Enes Ilhan <enilhan@k1m10s03.42istanbul.com.tr>
2026-09-06 20:27:29 +0300

    add: defensiveStrategy class has been created

--- ex2/strategy.py
+++ ex2/strategy.py
@@ -42,4 +42,13 @@ class AggressiveStrategy(BattleStrategy):
 
 
 class DefensiveStrategy(BattleStrategy):
-    pass
+    def is_valid(self, creature: Creature) -> bool:
+        return isinstance(creature, HealCapability)
+
+    def act(self, creature: Creature) -> None:
+        if not self.is_valid(creature):
+            raise InvalidStrategyError(creature.name, "defensive")
+
+        if isinstance(creature, HealCapability):
+            print(creature.attack())
+            print(creature.heal())
